7ed15f9e582cb3d8743bb286b7e7324657bd7fbf
westsider <whansen1@mac.com>
2025-10-24 13:15:20 -0500

    Initial Commit

---
+++ EconomicTradeSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EconomicTradeSystem
+//
+//  Created by Warren Hansen on 10/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EconomicTradeSystem/EconomicTradeSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  EconomicTradeSystemApp.swift
+//  EconomicTradeSystem
+//
+//  Created by Warren Hansen on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EconomicTradeSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
